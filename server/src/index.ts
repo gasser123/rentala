5b028fdb2b4615c767aea70efd0a5317ed60fd5a
Gasser Magdy <gassermagdy19@gmail.com>
2026-01-26 13:53:51 +0000

    added ecosystem config file for PM2 for deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,7 +32,7 @@ app.use("/leases", leaseRoutes);
 app.use("/applications", applicationRoutes);
 
 /* Server */
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 8080;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
